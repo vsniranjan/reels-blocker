c94460caabaf5267cfb3738332c3c37d558e39e1
Niranjan V S <niranjan.veluthamessery@gmail.com>
2026-07-26 22:27:35 +0530

    Bump to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "dev.niranjan.reelsblocker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     signingConfigs {
